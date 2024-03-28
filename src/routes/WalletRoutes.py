7868903dcbb066063abbd29b3d1510b221a6b943
Steven-Padilla <steven_padilla728@outlook.com>
2024-03-28 12:41:04 -0600

    feat: save new wallet move

--- src/routes/WalletRoutes.py
+++ src/routes/WalletRoutes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 # from src.database.db import get_connection_servicecode_orm
 from src.services.WalletService import WalletService
-from src.utils.errors.CustomException import CustomException, DataTypeException, MissingKeyException 
+from src.utils.errors.CustomException import CustomException, DataTypeException, MissingDataException, MissingKeyException 
 from src.utils.Security import Security 
 from orm_models import Users, UsersCentral, DigitalWallet
 # from sqlalchemy.orm import scoped_session, sessionmaker
@@ -26,15 +26,49 @@ def get_wallets_by_club():
 
 @main.route('/balance/', methods=['POST'], strict_slashes=False)
 def get_balance_by_userId():
+    has_access = Security.verify_token(request.headers)
+    if has_access is False:
+        response = jsonify({'message': 'Unauthorized', 'success': False})
+        return response, 401
     try:
         service_code = request.json['service_code']
         user_id = request.json['user_id']
-        print(user_id)
         response=WalletService.get_balance_by_user_id(service_code,user_id)
         return response
     except CustomException as ex:
         print(str(ex))
         return CustomException(ex)
+    
+@main.route('/save', methods=['POST'], strict_slashes=False)
+def save_new_wallet_move():
+    has_access = Security.verify_token(request.headers)
+    if has_access is False:
+        response = jsonify({'message': 'Unauthorized', 'success': False})
+        return response, 401
+    try:
+        body= request.json
+        required_keys=["service_code","amount","mode","type","concept","user_id"]
+        for key in required_keys:
+            if body.get(key) is None:
+                raise MissingKeyException(missing_key=key)
+
+        service_code=body["service_code"]
+        amount=body["amount"]
+        mode=body["mode"]
+        move_type=body["type"]
+        concept=body["concept"]
+        user_id=body["user_id"]
+        response =WalletService.save_new_balance( service_code,amount,mode,move_type,concept,user_id)
+        return response
+        
+
+    except CustomException as ex:
+        print(str(ex))
+        return CustomException(ex)
+    except MissingKeyException as ex:
+        print(str(ex))
+        return jsonify({'message': "Ups, algo salió mal", 'success': False})
+
 """
 TODO: Creación de Módulo "Monedero"
     - Traer registros de monedero por usuario
--- src/services/WalletService.py
+++ src/services/WalletService.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from src.database.db import get_connection_servicecode_orm
-from src.utils.errors.CustomException import CustomException, DataTypeException, MissingKeyException 
+from src.utils.errors.CustomException import CustomException, DataTypeException, MissingDataException, MissingKeyException 
 from orm_models import Users, UsersCentral, DigitalWallet
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -56,7 +56,6 @@ class WalletService:
         try: 
             db_session=this.get_db_session(service_code)
             wallet_moves=db_session.query(DigitalWallet).where(DigitalWallet.user_id == user_id).all()
-            print(wallet_moves)
             if not wallet_moves:
                 return jsonify({'data': [], 'success': True})
 
@@ -70,3 +69,30 @@ class WalletService:
         except CustomException as ex:
             print(str(ex))
             return CustomException(ex)
+    @classmethod
+    def save_new_balance(this, service_code,amount,mode,move_type,concept,user_id):
+        try:
+            engine = get_connection_servicecode_orm(service_code)
+            db_session=scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+            
+            user=db_session.query(Users).where(Users.id==user_id).first()
+            if user is None:
+                raise MissingDataException(Users.__tablename__,engine.url.database,user_id)
+            
+            previous_balance=user.wallet_balance
+            current_balance= previous_balance + amount if move_type == 0 else previous_balance - amount 
+
+
+            move=DigitalWallet(amount=amount, mode=mode, type=move_type,concept=concept, user_id=user_id, previous_balance=previous_balance, current_balance=current_balance)
+            user.wallet_balance=current_balance
+
+            db_session.add(move)
+            db_session.commit()
+            return jsonify({"data":move.to_dict(), "success":True})
+            
+        except CustomException as ex:
+            print(str(ex))
+            return CustomException(ex)  
+        finally:
+            if db_session:
+                db_session.close()
