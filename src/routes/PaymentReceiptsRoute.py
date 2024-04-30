38acc94c76e445751d90465cbdc7f76e145d8c1c
Steven-Padilla <steven_padilla728@outlook.com>
2024-04-29 23:18:15 -0600

    fix: single receipts and all receipts

--- src/routes/PaymentReceiptsRoute.py
+++ src/routes/PaymentReceiptsRoute.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from src.database.db import get_connection_servicecode_orm
 from src.models import Membership, PaymentDiscountMembership, PaymentReceipt, PaymentReceiptDescription, PaymentDiscount, Discount, Packages, Payment
+from src.models.payment_receipt_image import PaymentReceiptImage
 from src.utils.errors.CustomException import CustomException, DataTypeException, MissingKeyException 
 from src.utils.Security import Security 
 from orm_models import Users, UsersCentral
@@ -24,35 +25,58 @@ def get_single_receipts_v2():
                     PaymentReceipt,
                     PaymentReceiptDescription, 
                     PaymentDiscount,
+                    Discount,
+                    PaymentReceiptImage
                     )
                     .where(PaymentReceipt.idnotapago == receipt_id)
                     .join(PaymentReceiptDescription, PaymentReceipt.idnotapago == PaymentReceiptDescription.idnotapago)
                     .outerjoin(PaymentDiscount, PaymentDiscount.idnotapago == PaymentReceiptDescription.idnotapago)
-                    .first())
-                    # .join(PaymentReceiptDescription, PaymentReceipt.idnotapago == PaymentReceiptDescription.idnotapago)
-            membershipPay=db_session.query(PaymentDiscountMembership).where(PaymentDiscountMembership.id_payment_receipt==receipt_id).first()
+                    .outerjoin(Discount, Discount.iddescuento == PaymentDiscount.iddescuento)
+                    .outerjoin(PaymentReceiptImage, PaymentReceiptImage.payment_receipt_id == receipt_id)
+                    .all())
+            receiptData=[]
 
+            for receiptInfo, description, paymentDiscount,discount,image   in receipt:
+                membershipPay=db_session.query(PaymentDiscountMembership).where(PaymentDiscountMembership.id_payment_receipt==receipt_id).first()
+                motivoMembresia=""
+                if  membershipPay is not None:
+                    aux=db_session.query(Membership.title).where(Membership.id_membership == membershipPay.id_membership).first()
+                    motivoMembresia=aux[0]
 
-            [receiptInfo, description, discount]  = receipt
-            membershipDiscount=int(membershipPay.amount_to_discount) if membershipPay else None 
-            receiptData={
-                "idNotaPago":receiptInfo.idnotapago,
-                "descripcion":description.descripcion,
-                "fecha":receiptInfo.fecha,
-                "folio":receiptInfo.folio,
-                "fechaFactura":receiptInfo.fechafactura,
-                "folioFactura":receiptInfo.foliofactura,
-                "requiereFactura":receiptInfo.requierefactura,
-                "total":receiptInfo.total,
-                "subTotal":receiptInfo.subtotal,
-                "comision":receiptInfo.comisiontotal,
-                "descuento":None if not discount else int(discount.montoadescontar),
-                "montoMonedero":receiptInfo.montomonedero,
-                "metodoPago":receiptInfo.tipopago,
-                "estatus":receiptInfo.estatus,
-                "montoMembresia":membershipDiscount,
-            }
-            return jsonify({'data': receiptData, 'success': True})
+                membershipDiscount=int(membershipPay.amount_to_discount) if membershipPay else None
+                receiptData.append({
+                    "idNotaPago":receiptInfo.idnotapago,
+                    "descripcion":description.descripcion,
+                    "fecha":receiptInfo.fecha,
+                    "folio":receiptInfo.folio,
+                    "fechaFactura":receiptInfo.fechafactura,
+                    "folioFactura":receiptInfo.foliofactura,
+                    "requiereFactura":receiptInfo.requierefactura,
+                    "total":receiptInfo.total,
+                    "subTotal":receiptInfo.subtotal,
+                    "comision":receiptInfo.comisiontotal,
+                    "montoDescuento":"" if not paymentDiscount else int(paymentDiscount.montoadescontar),
+                    "imagen": ""if not image else  image.image,
+                    "motivoDescuento":"" if not discount else discount.titulo, 
+                    "montoMonedero":receiptInfo.montomonedero,
+                    "metodoPago":receiptInfo.tipopago,
+                    "estatus":receiptInfo.estatus,
+                    "motivoMembresia":motivoMembresia,
+                    "montoMembresia":membershipDiscount,
+                })
+            finalJson= {key: value for key, value in receiptData[0].items() if key not in ["descripcion", "motivoMembresia", "motivoDescuento"]}
+            finalJson.update({"descripcion":[]})
+            finalJson.update({"motivoMembresia":[]})
+            finalJson.update({"motivoDescuento":[]})
+            for singleReceipt in receiptData:
+                if singleReceipt.get("descripcion") not in finalJson.get("descripcion"):
+                    finalJson["descripcion"].append(singleReceipt.get("descripcion"))
+                if singleReceipt.get("motivoMembresia") not in finalJson.get("motivoMembresia"):
+                    finalJson["motivoMembresia"].append(singleReceipt.get("motivoMembresia"))
+                if singleReceipt.get("motivoDescuento") not in finalJson.get("motivoDescuento"):
+                    finalJson["motivoDescuento"].append(singleReceipt.get("motivoDescuento"))
+
+            return jsonify({'data': finalJson, 'success': True})
         except CustomException as ex:
             print(str(ex))
             return CustomException(ex)
@@ -72,28 +96,17 @@ def get_receipts_v2():
                 db_session.query(
                 PaymentReceipt,
                 PaymentReceiptDescription,
-                Payment,
-                PaymentDiscount,
-                Discount,
-                Packages,
             )
-                .join(PaymentReceiptDescription, PaymentReceipt.idnotapago == PaymentReceiptDescription.idnotapago)
-                .outerjoin(Payment, PaymentReceiptDescription.idpago == Payment.id)
-                .outerjoin(PaymentDiscount, PaymentDiscount.idpago == PaymentReceiptDescription.idpago)
-                .outerjoin(Discount, Discount.iddescuento == PaymentDiscount.iddescuento)
-                .outerjoin(Packages, Packages.idpaquete == PaymentReceiptDescription.idpaquete)).limit(10).all() 
+                .join(PaymentReceiptDescription, PaymentReceipt.idnotapago == PaymentReceiptDescription.idnotapago)).all() 
+
+            users=UsersCentral.query.all()
+            user_names={user.id : f'{user.name} {user.lastname} {user.secondsurname}' for user in users} 
             recipes_dics=[]
-            
-            for receipt, description, payment, discount, discount_name, packages in recipes:
-                print(description.to_dict())
-                user=UsersCentral.query.filter_by(id=receipt.idusuario).first()
-                userName="Sin nombre"
-                if user:
-                    userName=user.fullname()
+            for receipt, description in recipes:
                 recipes_dics.append({
                     "idRecibo":receipt.idnotapago,
                     "idUsuario":receipt.idusuario,
-                    "nombreUsuario":userName,
+                    "nombreUsuario":user_names.get(receipt.idusuario,"Sin nombre"),
                     "folio":receipt.folio,
                     "total":receipt.total,
                     "descripcion":description.descripcion,
